8bda374a73e2e3e9264ff964d6d0eea08b27f917
unknown <mellocay@gmail.com>
2020-05-25 22:02:31 -0700

    a mess of js

--- pages/script.js
+++ pages/script.js
@@ -1,8 +1,42 @@
-let button_edit = document.querySelector(".button_edit");
+let buttonEdit = document.querySelector(".button_edit");
 let popup = document.querySelector(".popup");
 
-function popping () {
+function popUnpop () {
   popup.classList.toggle("popup_active");
 }
 
-button_edit.addEventListener("click", popping);
+buttonEdit.addEventListener("click", popUnpop);
+
+let buttonClose = document.querySelector(".button_close");
+
+let buttonSave = document.querySelector(".button_save");
+
+buttonClose.addEventListener("click", popUnpop);
+buttonSave.addEventListener("click", popUnpop);
+
+let form = document.querySelector(".popup__form");
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+
+  let input = document.querySelector(".popup__input");
+  let nameInput = document.querySelector(".popup__input_name");
+  let occupationInput = document.querySelector(".popup__input_occupation");
+
+  var name = document.querySelector("nameInput.value");
+  var occupation = document.querySelector("occupationInput.value");
+
+  function replaceNameInput () {
+    nameInput.value.textContent = name;
+  }
+  
+  replaceNameInput ();
+
+  function replaceOccupationInput () {
+    occupationInput.value.textContent = occupation;
+  }
+
+  replaceOccupationInput ();
+}
+
+form.addEventListener("submit", formSubmitHandler);
