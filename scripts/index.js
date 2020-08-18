07a839ff3202eac905086c0f7f900842bfcfc628
unknown <mellocay@gmail.com>
2020-08-18 07:31:31 -0700

    everything is functioning again

--- scripts/index.js
+++ scripts/index.js
@@ -73,7 +73,7 @@ const editForm = new PopupWithForm(".popup_type_edit-button", (data) => {
 });
 
 const addForm = new PopupWithForm(".popup_type_add-button", (data) => {
-  const newCard = new Card(data, ".card__template", (data) => {
+  const newCard = new Card({name: data.title, link:data["image-link"]}, ".card__template", (data) => {
     imagePopup.open(data);
   });
     const cardElement = newCard.generateCard();
