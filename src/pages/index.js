859394495c8b2b1ffce19e2892a62441dc131027
unknown <mellocay@gmail.com>
2020-09-28 23:17:13 -0700

    bug: showCard function

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,33 +16,6 @@ const api = new Api({
   }
 });
 
-api.getAppInfo().then(([userData, initialCardsData]) => {
-  const userId = userData._id;
-  const cardGrid = new Section({
-    items: initialCardsData,
-    renderer: (data) => {
-      const card = new Card({
-        data,
-        handleCardClick: () => {
-          imagePopup.open(data);
-        },
-        handleDeleteClick: (cardId) => {
-          api.removeCard(cardId).then(() => {})
-        },
-      }, 
-      userId,
-      cardSelector);      
-      cardGrid.addItem(card.generateCard());
-    },
-  }, cardsConfig.placesWrap
-  );
-
-  cardGrid.renderItems();
-
-function handleDeleteClick(cardId) {
-  return api.removeCard(cardId);
-}
-
 const deleteForm = new PopupWithForm({
   popupElement: document.querySelector(popupConfig.popupDelete),
   handleFormSubmit: (cardId) => {
@@ -51,49 +24,66 @@ const deleteForm = new PopupWithForm({
   }
 });
 deleteForm.setEventListeners();
+
+const imagePopup = new PopupWithImage(popupImage);
+imagePopup.setEventListeners();
+
+api.getAppInfo().then(([userData, initialCardsData]) => {
+  const userId = userData._id;
+  const cardGrid = new Section({
+    items: initialCardsData,
+    renderer: showCard
+  }, cardsConfig.placesWrap
+  );
+ 
+  cardGrid.renderItems();
+
+  const addForm = new PopupWithForm({
+    popupElement: document.querySelector(popupConfig.popupAdd), 
+    handleFormSubmit: (data) => {
+      api.addCard(data).then(data => {
+        showCard(data);
+      })
+    }
+  });
+   
+  addForm.setEventListeners();
+  
+  buttonAdd.addEventListener("click", () => {
+    addForm.open();
+  });
+
+  function handleDeleteClick(cardId) {
+    return api.removeCard(cardId);
+  }
+
+  function showCard(data) {
+    const card = new Card({
+      data,
+      handleCardClick: () => {
+        imagePopup.open(data);
+      },
+      handleDeleteClick: (cardId) => {
+        api.removeCard(cardId).then(() => {});
+      },
+    }, 
+    userId,
+    cardSelector);  
+
+  cardGrid.addItem(card.generateCard());
+}
+
 // buttonDelete.addEventListener("click", () => {
 //   debugger;
 //   popupDelete.open(cardId);
 //   api.removeCard(cardId).then(() => {})
 // })
 
-  const addForm = new PopupWithForm({
-    popupElement: document.querySelector(popupConfig.popupAdd), 
-    handleFormSubmit: (data) => {
-      api.addCard(data).then(res => {
-      const card = new Card({
-        data, 
-        handleCardClick: () => {
-          imagePopup.open(data);
-        }
-      }, 
-      userId,
-      cardSelector);
-  // const cardElement = card.generateCard();
-  cardGrid.addItem(card.generateCard())
-  });
-    }
-  });
-
-//   showCard(data)
- 
-  addForm.setEventListeners();
-
-  buttonAdd.addEventListener("click", () => {
-    addForm.open();
-  });
-})
-
-
 const newProfile = new UserInfo(".profile__name", ".profile__occupation");
 
 api.getUserInfo().then(res => {
-  newProfile.setUserInfo({userName: res.name, userOccupation: res.about})
-})
-
-const imagePopup = new PopupWithImage(popupImage);
-imagePopup.setEventListeners();
-
+  newProfile.setUserInfo({userName: res.name, userOccupation: res.about});
+});
 
 const editProfileValidator = new FormValidator(defaultConfig, formEdit);
 editProfileValidator.enableValidation();
@@ -106,9 +96,9 @@ const editForm = new PopupWithForm({
     api.setUserInfo({name: data.name, about: data.occupation})
     .then(res => {
       newProfile.setUserInfo({userName: data.name, userOccupation:  data.occupation});
-    })
+    });
   }
-})
+});
    
 buttonEdit.addEventListener("click", () => {
   const profileInfo = newProfile.getUserInfo();
